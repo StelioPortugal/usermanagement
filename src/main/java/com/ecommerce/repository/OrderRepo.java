b2060a184129c4f22dae37eb5ecf85724d89e279
StelioPortugal <122265947+StelioPortugal@users.noreply.github.com>
2023-08-16 11:00:43 +0200

    code 2

--- src/main/java/com/ecommerce/repository/OrderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepo extends JpaRepository<Order,Integer> {
-}
--- src/main/java/com/ecommerce/service/CartService.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.service;
-
-import com.ecommerce.entity.Cart;
-import com.ecommerce.entity.Product;
-public interface CartService {
-
-    Cart addItemToCart(Product product, String username);
-}
